07f37e08a156aef2e8a9dd0d95c25264fd5f73fd
Camilo Romero <militoromero10@gmail.com>
2020-01-26 13:01:50 -0500

    refactoring map request owner

--- clinic-web/src/main/java/com/milo/pet/clinic/controller/OwnerController.java
+++ clinic-web/src/main/java/com/milo/pet/clinic/controller/OwnerController.java
@@ -3,7 +3,8 @@ package com.milo.pet.clinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/owners")
+@Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
     @RequestMapping({"","/","/index","/index.html"})
