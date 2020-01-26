fe816e4612a7a3364c6383cf197f76a5512689e7
Camilo Romero <militoromero10@gmail.com>
2020-01-26 15:39:22 -0500

    agregado auto incremento de Id en los serviceMap

--- clinic-data/src/main/java/com/milo/pet/clinic/bootstrap/DataLoader.java
+++ clinic-data/src/main/java/com/milo/pet/clinic/bootstrap/DataLoader.java
@@ -24,26 +24,22 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Camilo");
         owner1.setLastName("Romero");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Paula");
         owner2.setLastName("Romero");
         ownerService.save(owner2);
         System.out.println("Loaded owners ...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Jose");
         vet1.setLastName("Romero");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sebastian");
         vet2.setLastName("Romero");
         vetService.save(vet2);
