aa382226f09e6bffbc18e7ea5af9f90cbf5e2826
Camilo Romero <militoromero10@gmail.com>
2020-01-26 12:07:08 -0500

    refactorizado servicios en una principal

--- clinic-data/src/main/java/com/milo/pet/clinic/service/PetService.java
+++ clinic-data/src/main/java/com/milo/pet/clinic/service/PetService.java
@@ -2,12 +2,6 @@ package com.milo.pet.clinic.service;
 
 import com.milo.pet.clinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 
 }
