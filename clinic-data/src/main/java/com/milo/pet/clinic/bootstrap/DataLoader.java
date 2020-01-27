1f3482ef9b0cb34d94ae10eb4c69a327bdf7f7df
Camilo Romero <militoromero10@gmail.com>
2020-01-26 21:42:40 -0500

    PetType init OK

--- clinic-data/src/main/java/com/milo/pet/clinic/bootstrap/DataLoader.java
+++ clinic-data/src/main/java/com/milo/pet/clinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.milo.pet.clinic.bootstrap;
 
 import com.milo.pet.clinic.model.Owner;
+import com.milo.pet.clinic.model.PetType;
 import com.milo.pet.clinic.model.Vet;
 import com.milo.pet.clinic.service.OwnerService;
+import com.milo.pet.clinic.service.PetTypeService;
 import com.milo.pet.clinic.service.VetService;
 import com.milo.pet.clinic.service.map.OwnerServiceMap;
 import com.milo.pet.clinic.service.map.VetServiceMap;
@@ -14,14 +16,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Perro");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Gato");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Camilo");
