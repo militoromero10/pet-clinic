19166fcca5cd8d1f9c3c73fcdb4f3d9ad001c255
Camilo Romero <militoromero10@gmail.com>
2020-01-27 23:57:22 -0500

    added profiles to map, jpa and properties amended

--- clinic-data/src/main/java/com/milo/pet/clinic/service/map/PetTypeServiceMap.java
+++ clinic-data/src/main/java/com/milo/pet/clinic/service/map/PetTypeServiceMap.java
@@ -2,11 +2,13 @@ package com.milo.pet.clinic.service.map;
 
 import com.milo.pet.clinic.model.PetType;
 import com.milo.pet.clinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
--- clinic-data/src/main/java/com/milo/pet/clinic/service/map/VisitServiceMap.java
+++ clinic-data/src/main/java/com/milo/pet/clinic/service/map/VisitServiceMap.java
@@ -2,11 +2,13 @@ package com.milo.pet.clinic.service.map;
 
 import com.milo.pet.clinic.model.Visit;
 import com.milo.pet.clinic.service.VisitService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
