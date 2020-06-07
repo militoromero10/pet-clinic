f6a985f92593c289706f69ad1235f08c5ed5a6c3
Camilo Romero <militoromero10@gmail.com>
2020-06-07 00:08:39 -0500

    vet list and petservicemap test

--- clinic-web/src/main/java/com/milo/pet/clinic/controller/VetController.java
+++ clinic-web/src/main/java/com/milo/pet/clinic/controller/VetController.java
@@ -1,9 +1,14 @@
 package com.milo.pet.clinic.controller;
 
+import com.milo.pet.clinic.model.Vet;
 import com.milo.pet.clinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping({"/vets","/vets.html"})
@@ -21,4 +26,8 @@ public class VetController {
         return "vets/index";
     }
 
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
+    }
 }
